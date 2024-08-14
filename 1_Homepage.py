8a8ec2bb28d3d6e1750ee7398223e107ae42477c
BipinChowdary <136642309+BipinChowdary@users.noreply.github.com>
2024-08-14 15:48:54 +0530

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -59,13 +59,13 @@ col2.markdown('<h4 class="title2">Why Fuel Efficiency Prediction Is Useful ?</h4
 col2.write('Fuel efficiency prediction contributes significantly to pollution reduction by promoting the adoption of cleaner and more sustainable transportation options. By accurately forecasting fuel consumption, our project empowers individuals and businesses to make informed decisions that prioritize vehicles with lower emissions and higher fuel efficiency ratings. Moreover, our fuel efficiency prediction project facilitates the development and deployment of innovative technologies aimed at enhancing vehicle performance while minimizing environmental impact. Ultimately, our efforts in fuel efficiency prediction contribute to a cleaner, healthier planet for present and future generations.')
 
 with col2:
-st.write(" ")
-col1, col2 = st.columns(2)
-    
-with col1:
-    if st.button("💻 Open Project Code"):
-        webbrowser.open_new_tab("https://github.com/your_project_repo")
-    
-with col2:
-    if st.button("🌐 Visit My Portfolio"):
-        webbrowser.open_new_tab("https://bipinchowdary.github.io/")
+    st.write(" ")
+    col1, col2 = st.columns(2)
+        
+    with col1:
+        if st.button("💻 Open Project Code"):
+            webbrowser.open_new_tab("https://github.com/your_project_repo")
+        
+    with col2:
+        if st.button("🌐 Visit My Portfolio"):
+            webbrowser.open_new_tab("https://bipinchowdary.github.io/")
