5f06f2171e0323238c49e735f4e9bdadbd1ceeeb
BipinChowdary <136642309+BipinChowdary@users.noreply.github.com>
2024-08-14 15:16:25 +0530

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -23,14 +23,7 @@ custom_css = f"""
 }}
 .title {{
     font-family: 'TechFont', sans-serif;
-    font-size: 3rem; /* Adjust the font size as needed */
-    margin: 0;
-    padding: 0;
-    margin-bottom: 2rem;
-}}
-.title2 {{
-    font-family: 'TechFont', sans-serif;
-    font-size: 1.8rem; /* Adjust the font size as needed */
+    font-size: 3rem;
     margin: 0;
     padding: 0;
     margin-bottom: 2rem;
@@ -59,34 +52,10 @@ with col1:
         image = f.read()
     st.image(image, width=500)
 
-col2.markdown('<h4 class="title2">Why Fuel Efficiency Prediction Is Useful ?</h4><p class="gradient-divider"></p>', unsafe_allow_html=True)
+col2.markdown('<h4 class="title2">Why Fuel Efficiency Prediction Is Useful?</h4><p class="gradient-divider"></p>', unsafe_allow_html=True)
 col2.write('Fuel efficiency prediction contributes significantly to pollution reduction by promoting the adoption of cleaner and more sustainable transportation options. By accurately forecasting fuel consumption, our project empowers individuals and businesses to make informed decisions that prioritize vehicles with lower emissions and higher fuel efficiency ratings. Moreover, our fuel efficiency prediction project facilitates the development and deployment of innovative technologies aimed at enhancing vehicle performance while minimizing environmental impact. Ultimately, our efforts in fuel efficiency prediction contribute to a cleaner, healthier planet for present and future generations.')
 
-# Define navigation buttons
-with col2:
-    st.write(" ")
-    col1, col2, col3, col4, col5 = st.columns([2, 2.3, 2.4, 2.3, 3])
-
-    with col1:
-        if st.button("💻 CODE"):
-            st.session_state.page = "pages/6_Code"
-    
-    with col2:
-        if st.button(":car: Model Finder"):
-            st.session_state.page = "pages/2_Model_finder"
-    
-    with col3:
-        if st.button(":bar_chart: Charts"):
-            st.session_state.page = "pages/3_Charts"
-    
-    with col4:
-        if st.button("📝 Datasets"):
-            st.session_state.page = "pages/4_Dataset"
-    
-    with col5:
-        if st.button("🧑‍💻 Results"):
-            st.session_state.page = "pages/5_Results"
-
-# Handle page switching
-if 'page' in st.session_state:
-    st.experimental_rerun()
+# Single button to open a URL
+st.write(" ")
+if st.button("🔗 Open Project Page"):
+    st.markdown("[Click here to visit the project page](https://projectfuel.github.io)")
