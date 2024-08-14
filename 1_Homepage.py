74030f8a916ab6c34c52a7cd03e2ad5e96db1616
BipinChowdary <136642309+BipinChowdary@users.noreply.github.com>
2024-08-14 15:08:57 +0530

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -58,30 +58,31 @@ col2.markdown('<h4 class="title2">Why Fuel Efficiency Prediction Is Useful ?</h4
 col2.write('Fuel efficiency prediction contributes significantly to pollution reduction by promoting the adoption of cleaner and more sustainable transportation options. By accurately forecasting fuel consumption, our project empowers individuals and businesses to make informed decisions that prioritize vehicles with lower emissions and higher fuel efficiency ratings. Moreover, our fuel efficiency prediction project facilitates the development and deployment of innovative technologies aimed at enhancing vehicle performance while minimizing environmental impact. Ultimately, our efforts in fuel efficiency prediction contribute to a cleaner, healthier planet for present and future generations.')
 
 with col2:
-import streamlit as st
 
-col1, col2, col3, col4, col5 = st.columns([2, 2.3, 2.4, 2.3, 3])
+    st.write(" ")
 
-with col1:
-    if st.button("💻 CODE"):
-        st.session_state.page = "pages/6_Code"
+    col1, col2, col3, col4, col5 = st.columns([2, 2.3, 2.4, 2.3, 3])
 
-with col2:
-    if st.button(":car: Model Finder"):
-        st.session_state.page = "pages/2_Model_finder"
-
-with col3:
-    if st.button(":bar_chart: Charts"):
-        st.session_state.page = "pages/3_Charts"
-
-with col4:
-    if st.button("📝 Datasets"):
-        st.session_state.page = "pages/4_Dataset"
-
-with col5:
-    if st.button("🧑‍💻 Results"):
-        st.session_state.page = "pages/5_Results"
-
-# Function to switch pages based on session state
-if 'page' in st.session_state:
-    st.experimental_rerun()
+    with col1:
+        if st.button("💻 CODE"):
+            st.session_state.page = "pages/6_Code"
+    
+    with col2:
+        if st.button(":car: Model Finder"):
+            st.session_state.page = "pages/2_Model_finder"
+    
+    with col3:
+        if st.button(":bar_chart: Charts"):
+            st.session_state.page = "pages/3_Charts"
+    
+    with col4:
+        if st.button("📝 Datasets"):
+            st.session_state.page = "pages/4_Dataset"
+    
+    with col5:
+        if st.button("🧑‍💻 Results"):
+            st.session_state.page = "pages/5_Results"
+    
+    # Function to switch pages based on session state
+    if 'page' in st.session_state:
+        st.experimental_rerun()
