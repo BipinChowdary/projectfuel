09394910c7dac5014fb29009d5c10550a3d61180
BipinChowdary <136642309+BipinChowdary@users.noreply.github.com>
2024-08-14 15:11:22 +0530

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import base64
 
+# Set page configuration
 st.set_page_config(page_title="My Homepage", page_icon='🏡', layout="wide")
-st.markdown('<style>div.block-container{padding-top:2.5rem;}</style>',unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top:2.5rem;}</style>', unsafe_allow_html=True)
+
 def load_font(font_path):
     with open(font_path, "rb") as font_file:
         font_data = font_file.read()
     encoded_font = base64.b64encode(font_data).decode('utf-8')
     return encoded_font
 
+# Load custom font
 tech_font = load_font('fonts/TECH.ttf')
 
+# Define custom CSS
 custom_css = f"""
 <style>
 @font-face {{
@@ -41,26 +45,26 @@ custom_css = f"""
 </style>
 """
 
+# Apply custom CSS
 st.markdown(custom_css, unsafe_allow_html=True)
 st.markdown('<h1 class="title">🏡 Fuel Efficiency Prediction</h1>', unsafe_allow_html=True)
 
 st.write(' ')
-col1,_,col2 = st.columns([4,0.5,5.5])
 
-with open('images/header.png', 'rb') as f:
-    image = f.read()
-    col1.image(image, width=500)
+# Define columns for layout
+col1, _, col2 = st.columns([4, 0.5, 5.5])
 
+with col1:
+    with open('images/header.png', 'rb') as f:
+        image = f.read()
+    st.image(image, width=500)
 
-col2.markdown(custom_css, unsafe_allow_html=True)
 col2.markdown('<h4 class="title2">Why Fuel Efficiency Prediction Is Useful ?</h4><p class="gradient-divider"></p>', unsafe_allow_html=True)
-
 col2.write('Fuel efficiency prediction contributes significantly to pollution reduction by promoting the adoption of cleaner and more sustainable transportation options. By accurately forecasting fuel consumption, our project empowers individuals and businesses to make informed decisions that prioritize vehicles with lower emissions and higher fuel efficiency ratings. Moreover, our fuel efficiency prediction project facilitates the development and deployment of innovative technologies aimed at enhancing vehicle performance while minimizing environmental impact. Ultimately, our efforts in fuel efficiency prediction contribute to a cleaner, healthier planet for present and future generations.')
 
+# Define navigation buttons
 with col2:
-
     st.write(" ")
-
     col1, col2, col3, col4, col5 = st.columns([2, 2.3, 2.4, 2.3, 3])
 
     with col1:
@@ -82,7 +86,7 @@ with col2:
     with col5:
         if st.button("🧑‍💻 Results"):
             st.session_state.page = "pages/5_Results"
-    
-    # Function to switch pages based on session state
-    if 'page' in st.session_state:
-        st.experimental_rerun()
+
+# Handle page switching
+if 'page' in st.session_state:
+    st.experimental_rerun()
