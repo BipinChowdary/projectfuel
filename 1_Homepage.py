d05992b6d25b57679c415542eb816f5e2ad852d4
BipinChowdary <136642309+BipinChowdary@users.noreply.github.com>
2024-08-14 15:44:01 +0530

    Update 1_Homepage.py

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -58,7 +58,7 @@ col2.markdown('<h4 class="title2">Why Fuel Efficiency Prediction Is Useful ?</h4
 col2.write('Fuel efficiency prediction contributes significantly to pollution reduction by promoting the adoption of cleaner and more sustainable transportation options. By accurately forecasting fuel consumption, our project empowers individuals and businesses to make informed decisions that prioritize vehicles with lower emissions and higher fuel efficiency ratings. Moreover, our fuel efficiency prediction project facilitates the development and deployment of innovative technologies aimed at enhancing vehicle performance while minimizing environmental impact. Ultimately, our efforts in fuel efficiency prediction contribute to a cleaner, healthier planet for present and future generations.')
 
 with col2:
-st.write(" ")
+    st.write(" ")
     col1, col2 = st.columns(2)
     
     with col1:
